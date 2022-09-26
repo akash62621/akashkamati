58a70b096df40cbc481e4641bdd08a30d8730b12
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-09-26 21:10:53 +0530

    inital setup

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="headerContainer">
+        <p className="headerContainer__ptag">
+          Lets start with header
+          <button className="headerContainer__ptag__button">hello</button>
+        </p>
+      </div>
+    </>
+  );
+}
